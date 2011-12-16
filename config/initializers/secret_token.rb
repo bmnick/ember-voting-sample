ca756b67688cda2216c9feb7401b60af03e85e2a
Ben Nicholas <ben@ben-nicholas.com>
2011-12-16 16:15:42 -0500

    initial rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maplist::Application.config.secret_token = '1731a8c07290bf49586ca4dc57d97ecb78556896f7779c808adfdc1cb08175889cf43da247903b9f663e3d1daa87db3d926ea039c0f07d314ebe13985b2b7e5f'
