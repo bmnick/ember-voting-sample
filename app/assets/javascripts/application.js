c4a645b25bd6337150148db052c8164e51fbcbad
Ben Nicholas <ben@ben-nicholas.com>
2011-12-17 20:04:33 -0500

    Pull sorting out into a composite property

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,56 +6,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
-
-var MAPList = Em.Application.create();
-
-MAPList.AP = Em.Object.extend({
-  name: 'somebody',
-  score: 0,
-  
-  upvote: function() {
-    this.score += 1;
-    console.log("called");
-  },
-  
-  downvote: function() {
-    this.score -= 1;
-    console.log("down");
-  }
-});
-
-MAPList.APController = Em.ArrayProxy.create({
-  content: [],
-  
-  createAp: function(name) {
-    var ap = MAPList.AP.create({ name: name });
-    this.pushObject(ap);
-  }
-
-});
-
-MAPList.CreateAPView = Em.TextField.extend({
-  insertNewline: function() {
-    var val = this.get('value');
-    
-    if (val) {
-      MAPList.APController.createAp(val);
-      this.set('value', '');
-    }
-  }
-});
-
-MAPList.APView = Em.View.extend({
-  person: null,
-  
-  upvote: function() {
-    console.log("up");
-    this.person.set('score', this.person.get('score') + 1);
-  },
-  
-  downvote: function() {
-    console.log("down");
-    this.person.set('score', this.person.get('score') - 1);
-  }
-});
+//= require ember-0.9
+//= require votelist
---
+++ app/assets/javascripts/votelist.js
@@ -0,0 +1,77 @@
+VoteList = Em.Application.create();
+
+VoteList.Candidate = Em.Object.extend({
+  name: 'somebody',
+  score: 0,
+  
+  /* Increment the score by one
+   */
+  upvote: function() {
+    this.set('score', this.get('score') + 1);
+  },
+  
+  /* Decrement the score by one
+   */
+  downvote: function() {
+    this.set('score', this.get('score') - 1);
+  }
+});
+  
+VoteList.CandidateController = Em.ArrayController.create({
+  content: [],
+
+  /* Computed property to keep content sorted
+   */
+  sortedContent: function() {
+    var content = this.get('content');
+    
+    if (content) {
+      var result = Em.copy(content);
+      
+      result = result.sort( function( a, b ) {
+        return b.get('score') - a.get('score');
+      });
+      
+      return result;
+    } else {
+      return [];
+    }
+  }.property('@each.score').cacheable(),
+  
+  /* Creation method for adding a new canddiate
+   */
+  createCandidate: function(name) {
+    var cand = VoteList.Candidate.create({name: name});
+    this.pushObject(cand);
+  }
+});
+
+VoteList.CreateCandidateView = Em.TextField.extend({
+  /* Automate having new items inserted into the 
+   * list of candidates when enter is pressed
+   */
+  insertNewline: function() {
+    val = this.get('value');
+    
+    if (val) {
+      VoteList.CandidateController.createCandidate(val);
+      this.set('value', '');
+    }
+  }
+});
+
+VoteList.CandidateView = Em.View.extend({
+  person: null,
+  
+  /* View helper to pass calls down to the model
+   */
+  upvote: function() { 
+    this.person.upvote()
+  },
+  
+  /* View helper to pass calls down to the model
+   */
+  downvote: function() { 
+    this.person.downvote()
+  }
+});
